e7bc410c210b1cce0ec62da12a1c507ca90a0251
NataliiaKostiuk <128825690+NataliiaKostiuk@users.noreply.github.com>
2023-08-18 21:13:49 +0000

    Deploying to gh-pages from @ NataliiaKostiuk/goit-js-hw-09@00b2968c54c752e0074543adcd8e03f5f20de997 🚀

---
+++ 01-color-switcher.804b49df.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(o){const r=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3);e.disabled=!0,a.addEventListener("click",(()=>{clearInterval(r),e.disabled=!1}))}));
+//# sourceMappingURL=01-color-switcher.804b49df.js.map
---
+++ 01-color-switcher.9ff08ec3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(n){var o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);e.disabled=!0,a.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}))}))}();
+//# sourceMappingURL=01-color-switcher.9ff08ec3.js.map
